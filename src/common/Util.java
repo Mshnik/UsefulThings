790860b2506375507bcc2728aff5e41025202553
MShnik <Mgpshnik@gmail.com>
2015-03-10 12:39:53 -0400

    Fixed randomElement to account for incomming collection being null or empty

--- src/common/Util.java
+++ src/common/Util.java
@@ -10,10 +10,18 @@ public class Util {
 	 * HashSets.
 	 * <br><br>
 	 * Synchronizes on the given col to prevent concurrent modification.
+	 * Returns null if col is null or empty.
 	 */
 	public static <E> E randomElement(Collection<E> col){
+		if(col == null)
+			return null;
+		
 		Iterator<E> iterator;
 		synchronized(col){
+			
+			if(col.isEmpty())
+				return null;
+			
 			iterator = col.iterator();
 			int r = (int)(Math.random() * col.size());
 			for(int i = 0; i < r; i++){ iterator.next(); }

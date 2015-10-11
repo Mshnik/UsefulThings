52ae4873d6fdb01c59d9efe41e56875269df21a7
MShnik <Mgpshnik@gmail.com>
2015-10-11 17:13:12 -0400

    Added tests for JUnitUtil, deleted chunks of functionalUtil because of ambiguity

--- src/tests/common/JUnitUtilTest.java
+++ src/tests/common/JUnitUtilTest.java
@@ -1,8 +1,12 @@
 package common;
 
+import functional.Consumer;
+import functional.FunctionalUtil;
+import functional.Unit;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static functional.FunctionalUtil.*;
 import static org.junit.Assert.*;
 import static common.JUnitUtil.*;
 
@@ -23,11 +27,32 @@ public class JUnitUtilTest {
     assertEquals(66, JUnitUtil.testAll(assertEqualsFunc(1,1), assertEqualsFunc(2,2), assertEqualsFunc(2,3)));
   }
 
+  @Test
+  public void testShouldFail() {
+    shouldFail(JUnitUtilTest::fails);
+    shouldFail(migrate(JUnitUtilTest::runsIfNotZero), 0);
+    shouldFail(migrate(JUnitUtilTest::runsIfNeitherZero), 0, 1);
+    shouldFail(migrate(JUnitUtilTest::runsIfNeitherZero), 1, 0);
 
+    shouldFail(JUnitUtilTest::fails, RuntimeException.class);
+    shouldFail(migrate(JUnitUtilTest::runsIfNotZero), RuntimeException.class, 0);
+    shouldFail(migrate(JUnitUtilTest::runsIfNeitherZero), RuntimeException.class, 0, 1);
+    shouldFail(migrate(JUnitUtilTest::runsIfNeitherZero), RuntimeException.class, 1, 0);
+  }
 
   private static void fails() throws RuntimeException {
     throw new RuntimeException();
   }
 
-  private static void runs(){}
+  private static void runsIfNotZero(int arg) throws RuntimeException {
+    if (arg == 0){
+      throw new RuntimeException();
+    }
+  }
+
+  private static void runsIfNeitherZero(int arg1, int arg2) throws  RuntimeException {
+    if (arg1 == 0 || arg2 == 0) {
+      throw new RuntimeException();
+    }
+  }
 }

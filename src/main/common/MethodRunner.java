20534fc1e1a0631b946594dec6017e4f2c1ffdb7
MShnik <Mgpshnik@gmail.com>
2015-10-21 12:40:25 -0400

    Added new constructors and completion time check for MethodRunner, added testing methods for MethodRunner

--- src/main/common/MethodRunner.java
+++ src/main/common/MethodRunner.java
@@ -1,9 +1,7 @@
 package common;
 
 import common.types.Either;
-import common.types.Left;
-import common.types.Right;
-import functional.Supplier;
+import functional.*;
 
 /** MethodRunner allows a method call to be made along with a timeout time.
  * The method call is made in a newly created thread while the calling thread waits for it to
@@ -11,6 +9,10 @@ import functional.Supplier;
  * If, however, the timeout time arrives without the method call finished, the calling thread is
  * simply terminated in an unsafe state. This was done in an effort to make MethodRunner simplistic
  * and easy to use, but has concequences if methods with side-effects.
+ * <br><br>
+ * MethodRunners are not meant to be shared within threads. Multi-threaded behavior is unsafe and
+ * completely unspecified.
+ *
  * @param <T> - the return type of the method call
  * @author Mshnik
  */
@@ -21,9 +23,24 @@ public class MethodRunner<T> {
   private Throwable throwable;
   private boolean halted;
   private boolean done;
-  private final long millisToWait;
 
+  private final long millisToWait;
+  private long completionMillis;
+
+  /**
+   * The default max amount of milliseconds to wait for the operation to finish, if
+   * millisToWait is not provided in construction.
+   */
+  public static final long DEFAULT_WAIT_TIME = 1000;
   private static final long MILLIS_SLEEP_BETWEEN_UPDATES = 100;
+
+  /** Constructs a new MethodRunner that takes at most DEFAULT_WAIT_TIME ms.
+   * @param methodCall - the method call to make
+   * @throws IllegalArgumentException if methodCall == null
+   */
+  public MethodRunner(Supplier<T> methodCall) throws IllegalArgumentException {
+    this(methodCall, DEFAULT_WAIT_TIME);
+  }
 
   /** Construct a new MethodRunner
    * @param methodCall - the method call to make
@@ -40,6 +57,70 @@ public class MethodRunner<T> {
 
     this.methodCall = methodCall;
     this.millisToWait = millisToWait;
+    this.completionMillis = -1;
+  }
+
+  /** Construct a new MethodRunner that takes at most DEFAULT_WAIT_TIME ms.
+   * @param methodCall - the method call to make
+   * @param arg - the argument to give to the method call
+   * @throws IllegalArgumentException - if methodCall == null, or millisToWait <= 0
+   */
+  public <A> MethodRunner(Function<A, T> methodCall, A arg) throws IllegalArgumentException {
+    this(methodCall.partialApply(arg));
+  }
+
+  /** Construct a new MethodRunner
+   * @param methodCall - the method call to make
+   * @param arg - the argument to give to the method call
+   * @param millisToWait - the number of milliseconds to wait for methodCall to return
+   * @throws IllegalArgumentException - if methodCall == null, or millisToWait <= 0
+   */
+  public <A> MethodRunner(Function<A, T> methodCall, A arg, long millisToWait) throws IllegalArgumentException {
+    this(methodCall.partialApply(arg), millisToWait);
+  }
+
+  /** Construct a new MethodRunner that takes at most DEFAULT_WAIT_TIME ms.
+   * @param methodCall - the method call to make
+   * @param arg - the first argument to give to the method call
+   * @param arg2 - the second argument to give to the method call
+   * @throws IllegalArgumentException - if methodCall == null, or millisToWait <= 0
+   */
+  public <A,B> MethodRunner(BiFunction<A, B, T> methodCall, A arg, B arg2) throws IllegalArgumentException {
+    this(methodCall.partialApply(arg, arg2));
+  }
+
+  /** Construct a new MethodRunner
+   * @param methodCall - the method call to make
+   * @param arg - the first argument to give to the method call
+   * @param arg2 - the second argument to give to the method call
+   * @param millisToWait - the number of milliseconds to wait for methodCall to return
+   * @throws IllegalArgumentException - if methodCall == null, or millisToWait <= 0
+   */
+  public <A,B> MethodRunner(BiFunction<A, B, T> methodCall, A arg, B arg2, long millisToWait) throws IllegalArgumentException {
+    this(methodCall.partialApply(arg, arg2), millisToWait);
+  }
+
+  /** Construct a new MethodRunner that takes at most DEFAULT_WAIT_TIME ms.
+   * @param methodCall - the method call to make
+   * @param arg - the first argument to give to the method call
+   * @param arg2 - the second argument to give to the method call
+   * @param arg3 - the third argument to give to the method call
+   * @throws IllegalArgumentException - if methodCall == null, or millisToWait <= 0
+   */
+  public <A,B,C> MethodRunner(TriFunction<A, B, C, T> methodCall, A arg, B arg2, C arg3) throws IllegalArgumentException {
+    this(methodCall.partialApply(arg, arg2, arg3));
+  }
+
+  /** Construct a new MethodRunner
+   * @param methodCall - the method call to make
+   * @param arg - the first argument to give to the method call
+   * @param arg2 - the second argument to give to the method call
+   * @param arg3 - the third argument to give to the method call
+   * @param millisToWait - the number of milliseconds to wait for methodCall to return
+   * @throws IllegalArgumentException - if methodCall == null, or millisToWait <= 0
+   */
+  public <A,B,C> MethodRunner(TriFunction<A, B, C, T> methodCall, A arg, B arg2, C arg3, long millisToWait) throws IllegalArgumentException {
+    this(methodCall.partialApply(arg, arg2, arg3), millisToWait);
   }
 
   /** Runs the method call for up to {@code millisToWait} milliseconds,
@@ -50,22 +131,25 @@ public class MethodRunner<T> {
    * @return the result if it was computed, the throwable if an exception or error occurred, or null if
    *    the call was terminated due to timeout or if the worker thread was somehow interrupted.
    */
-  public Either<Throwable,T> runAndGet() {
+  private Either<Throwable,T> runAndGet() {
     done = false;
     halted = false;
     result = null;
     throwable = null;
+    completionMillis = -1;
 
-    long startTime = System.currentTimeMillis();
     TimeoutWorker r = new TimeoutWorker();
+    long startTime = System.currentTimeMillis();
     r.start();
     while (System.currentTimeMillis() < startTime + millisToWait && !done) {
       try {
         Thread.sleep(MILLIS_SLEEP_BETWEEN_UPDATES);
       } catch (InterruptedException e) {
-        return null; //Give up on result
+        throw new RuntimeException("Worker thread was interrupted, somehow. Bad.");
       }
     }
+
+    completionMillis = System.currentTimeMillis() - startTime;
 
     if(!done) {
       r.stop();
@@ -92,6 +176,39 @@ public class MethodRunner<T> {
     } else {
       return Either.selectNonNull(throwable, result);
     }
+  }
+
+  /** If his method call has not been run, calls runAndGet() to compute and return it.
+   * If it has, returns the result that was computed.
+   * Thus multiple calls to get() will compute the result once and then return it for each call.
+   * If the method call resulted in an exception or error, re-throws that exception or error.
+   * Otherwise, returns the result that the method call gave.
+   *
+   * @throws Throwable, whatever Throwable the method call threw.
+   * @return the result if computed, or null if the method call was terminated due to timeout
+   */
+  public T getOrThrow() throws Throwable {
+    if (!done) {
+      Either<Throwable, T> e = runAndGet();
+      if (e == null) return null;
+      else if (e.isLeft()) throw e.asLeft();
+      else return e.asRight();
+    } else if (halted) {
+      return null;
+    } else {
+      if (throwable != null) throw throwable;
+      else return result;
+    }
+  }
+
+  /** Return the amount of time it took this method runner to run.
+   *  If this methodRunner hasn't been run yet or is in the process of running, return -1.
+   *  If this wasn't terminated due to infinite loop, the returned value will be less than
+   *  the allotted time. If it was terminated due to infinite loop, the returned value
+   *  will be >= the allotted time
+   */
+  public long getCompletionMillis() {
+    return completionMillis;
   }
 
   /** A helper class for MethodRunner - simply executes the method call and sets done to
---
+++ src/tests/common/MethodRunnerTest.java
@@ -0,0 +1,142 @@
+package common;
+
+import functional.BiFunction;
+import functional.Function;
+import functional.Supplier;
+import org.junit.Test;
+import static common.JUnitUtil.*;
+import static org.junit.Assert.*;
+
+public class MethodRunnerTest {
+
+
+  @Test
+  public void testConstruction() {
+    Function<Supplier<Object>, MethodRunner<Object>> c = MethodRunner::new;
+    BiFunction<Supplier<Object>, Long, MethodRunner<Object>> c2 = MethodRunner::new;
+    shouldFail(c, null);
+    shouldFail(c2, null, -5L);
+    shouldFail(c2, null, 100L);
+    shouldFail(c2, () -> null, -5L);
+
+    try{
+      new MethodRunner<>(() -> null);
+      new MethodRunner<>(() -> null, 5);
+    }catch(IllegalArgumentException e) {
+      fail("Valid construction failed");
+    }
+  }
+
+  @Test
+  public void testValidRunning() {
+    MethodRunner<Integer> mR = new MethodRunner<>(() -> 10);
+
+    assertEquals(new Integer(10), mR.get().asRight());
+    try {
+      assertEquals(new Integer(10), mR.getOrThrow());
+    }catch(Throwable t) {
+      fail("Valid call threw throwable " + t);
+    }
+    assertTrue(mR.getCompletionMillis() < MethodRunner.DEFAULT_WAIT_TIME);
+
+    MethodRunner<Integer> mR2 = new MethodRunner<>(() -> 15);
+
+    assertEquals(new Integer(15), mR2.get().asRight());
+    try {
+      assertEquals(new Integer(15), mR2.getOrThrow());
+    }catch(Throwable t) {
+      fail("Valid call threw throwable " + t);
+    }
+
+    assertTrue(mR2.getCompletionMillis() < MethodRunner.DEFAULT_WAIT_TIME);
+  }
+
+  private static Integer throwsException() throws RuntimeException {
+    throw new RuntimeException("Message here");
+  }
+
+  @Test
+  public void testExceptionThrown() {
+    MethodRunner<Integer> mR = new MethodRunner<>(MethodRunnerTest::throwsException);
+    assertEquals("Message here", mR.get().asLeft().getMessage());
+
+    try {
+      mR.getOrThrow();
+      fail ("Throwing call didn't throw exception");
+    } catch(Throwable t) {
+      //Good
+    }
+    assertTrue(mR.getCompletionMillis() < MethodRunner.DEFAULT_WAIT_TIME);
+
+
+    MethodRunner<Integer> mR2 = new MethodRunner<>(MethodRunnerTest::throwsException);
+
+    try {
+      mR2.getOrThrow();
+      fail ("Throwing call didn't throw exception");
+    } catch(Throwable t) {
+      //Good
+    }
+    assertEquals("Message here", mR2.get().asLeft().getMessage());
+    assertTrue(mR2.getCompletionMillis() < MethodRunner.DEFAULT_WAIT_TIME);
+
+  }
+
+  private static Integer stackOverflows(boolean ok) {
+    if (ok) return 5;
+    else return stackOverflows(false);
+  }
+
+  @Test
+  public void testStackOverflow() {
+    MethodRunner<Integer> mR = new MethodRunner<>(MethodRunnerTest::stackOverflows, false);
+    assertEquals(StackOverflowError.class, mR.get().asLeft().getClass());
+
+    try {
+      mR.getOrThrow();
+      fail ("Stack overflowing call didn't throw exception");
+    } catch(Throwable t) {
+      //Good
+    }
+    assertTrue(mR.getCompletionMillis() < MethodRunner.DEFAULT_WAIT_TIME);
+
+    MethodRunner<Integer> mR2 = new MethodRunner<>(MethodRunnerTest::stackOverflows, false);
+
+    try {
+      mR2.getOrThrow();
+      fail ("Stack overflowing call didn't throw exception");
+    } catch(Throwable t) {
+      //Good
+    }
+    assertEquals(StackOverflowError.class, mR2.get().asLeft().getClass());
+    assertTrue(mR2.getCompletionMillis() < MethodRunner.DEFAULT_WAIT_TIME);
+  }
+
+  private static Integer loopForever() {
+    while(true){
+      //asdf
+    }
+  }
+
+  @Test
+  public void testInfiniteLoop() {
+    final long WAIT_TIME = 500L;
+    MethodRunner<Integer> mR = new MethodRunner<>(MethodRunnerTest::loopForever, WAIT_TIME);
+    assertEquals(null, mR.get());
+    try {
+      assertEquals(null, mR.getOrThrow());
+    }catch(Throwable t) {
+      fail("infinite loop call threw throwable " + t);
+    }
+    assertTrue(mR.getCompletionMillis() >= WAIT_TIME);
+
+    MethodRunner<Integer> mR2 = new MethodRunner<>(MethodRunnerTest::loopForever, WAIT_TIME);
+    try {
+      assertEquals(null, mR2.getOrThrow());
+    }catch(Throwable t) {
+      fail("infinite loop call threw throwable " + t);
+    }
+    assertEquals(null, mR2.get());
+    assertTrue(mR2.getCompletionMillis() >= WAIT_TIME);
+  }
+}

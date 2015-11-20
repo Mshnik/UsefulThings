e6883cac995697c9b3756707d2b5c51a0fb2a862
MShnik <Mgpshnik@gmail.com>
2015-11-20 02:17:25 -0500

    Got rid of packageinfos

--- src/main/common/dataStructures/util/packageinfo.java
+++
@@ -1,6 +0,0 @@
-package common.dataStructures.util;
-
-/** This util folder holds classes that are useful for implementing 
- * the classes in the dataStructures parent package.
- * @author Mshnik
- */

539933597f9242d2db18cfc2e4cda32baf36af52
MShnik <Mgpshnik@gmail.com>
2015-11-27 23:38:41 -0500

    Improved Object IO after Autograding use

--- src/main/grid/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * @author MPatashnik
- *
- */
-package grid;
--- src/main/io/ObjectIO.java
+++ src/main/io/ObjectIO.java
@@ -18,20 +18,13 @@ public class ObjectIO {
    * Creates a serialized Object file for input Object for later reading and editing
    *
    * @param toWrite   - the object to write to memory.
-   * @param directory - the location to write the object to
-   * @param name      - the name to give to the serialized version of this object.
-   *                  Must not contain the '.' or the '/' characters.
+   * @param filePathToWrite - the filePath to write with this object
    */
-  public static void write(Object toWrite, String directory, String name)
-      throws IllegalArgumentException, IOException {
-
-    if (name.contains(".") || name.contains("/"))
-      throw new IllegalArgumentException("Input name contains a '.' or a '/'");
-
+  public static void write(Object toWrite, String filePathToWrite) throws IOException {
     FileOutputStream fileOut = null;
     ObjectOutputStream out = null;
     try {
-      fileOut = new FileOutputStream(directory + "/" + name + EXTENSION);
+      fileOut = new FileOutputStream(filePathToWrite);
       out = new ObjectOutputStream(fileOut);
       out.writeObject(toWrite);
 
@@ -46,14 +39,64 @@ public class ObjectIO {
     }
   }
 
+  /**
+   * Creates a serialized Object file for input Object for later reading and editing
+   *
+   * @param toWrite   - the object to write to memory.
+   * @param directory - the location to write the object to
+   * @param name      - the name to give to the serialized version of this object.
+   *                  Must not contain the '.' or the '/' characters.
+   */
+  public static void write(Object toWrite, String directory, String name)
+      throws IllegalArgumentException, IOException {
+
+    if (name.contains(".") || name.contains("/"))
+      throw new IllegalArgumentException("Input name contains a '.' or a '/'");
+
+    write(toWrite, directory + "/" + name + EXTENSION);
+  }
+
   @SuppressWarnings("rawtypes")
+  /** Prompts the user to select a serialized object from memory to load.
+   * @param objectClass - The class of the object that the user is expected to return
+   * @param objectFileToLoad - The file containing the object to be loaded
+   * @return - The selected object, loaded from memory
+   * @throws RuntimeException - If the user selects the wrong class of object, or if null somehow results
+   */
+  public static Object load(Class objectClass, File objectFileToLoad) throws RuntimeException, IOException {
+    Object g = null;
+    FileInputStream fileIn = null;
+    ObjectInputStream in = null;
+    try {
+      fileIn = new FileInputStream(objectFileToLoad);
+      in = new ObjectInputStream(fileIn);
+
+      g = in.readObject();
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
+      //Make sure resources are closed
+      if (fileIn != null)
+        fileIn.close();
+      if (in != null)
+        in.close();
+    }
+
+    if (objectClass.isInstance(g))
+      return g;
+
+    throw new RuntimeException("Invalid Object Selection - found instance of " + (g == null ? "null " : g.getClass())
+        + " required " + objectClass);
+  }
+
   /** Prompts the user to select a serialized object from memory to load.
    * @param objectClass - The class of the object that the user is expected to return
    * @param directory - The location the user is prompted to select from, though they can navigate to other locations.
    * @return - The selected object, loaded from memory
    * @throws RuntimeException - If the user selects the wrong class of object.
    */
-  public static Object load(Class objectClass, String directory) throws RuntimeException, IOException {
+  public static Object chooseAndLoad(Class objectClass, String directory) throws RuntimeException, IOException {
     JFileChooser chooser = new JFileChooser(directory);
     chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
     FileNameExtensionFilter filter = new FileNameExtensionFilter(objectClass.getSimpleName() + " Objects", EXTENSION);
@@ -65,29 +108,9 @@ public class ObjectIO {
     ObjectInputStream in = null;
 
     if (returnVal == JFileChooser.APPROVE_OPTION) {
-      File f = chooser.getSelectedFile();
-      try {
-        fileIn = new FileInputStream(f);
-        in = new ObjectInputStream(fileIn);
-
-        g = in.readObject();
-
-      } catch (Exception e) {
-        e.printStackTrace();
-      } finally {
-        //Make sure resources are closed
-        if (fileIn != null)
-          fileIn.close();
-        if (in != null)
-          in.close();
-      }
-
+      return load(objectClass, chooser.getSelectedFile());
+    } else {
+      return null;
     }
-
-    if (objectClass.isInstance(g))
-      return g;
-
-    throw new RuntimeException("Invalid Object Selection - found instance of " + g.getClass() + " required " + objectClass);
   }
-
 }

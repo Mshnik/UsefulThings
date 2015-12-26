2d45d772b4bab8ff979b96f65119e02f5b848087
MShnik <Mgpshnik@gmail.com>
2015-12-26 00:00:42 -0500

    Made mux fields final

---
+++ src/main/concurrent/NWayJoin.java
@@ -0,0 +1,56 @@
+package concurrent;
+
+import java.util.ArrayList;
+
+public class NWayJoin implements SynchroBuffer {
+
+  private ArrayList<String> ids;
+  private Object[] locks;
+
+  private final Object wayInCondition;
+  private final Object wayOutCondition;
+  private int typesArrived;
+  private int typesLeaving;
+
+  public NWayJoin(String... typeIDs) {
+    ids = new ArrayList<>();
+    locks = new Object[typeIDs.length];
+    for(int i = 0; i < locks.length; i++) {
+      ids.add(typeIDs[i]);
+      locks[i] = new Object();
+    }
+    wayInCondition = new Object();
+    wayOutCondition = new Object();
+    typesArrived = 0;
+    typesLeaving = 0;
+  }
+
+  @Override
+  public synchronized void waitUntilReady(String arg) throws InterruptedException {
+    int index = ids.indexOf(arg);
+    synchronized (locks[index]) {
+      synchronized (wayInCondition) {
+        typesArrived++;
+        if (typesArrived == locks.length) {
+          typesLeaving = 0;
+          wayInCondition.notifyAll();
+        } else {
+          while (typesArrived < locks.length) {
+            wayInCondition.wait();
+          }
+        }
+      }
+      synchronized (wayOutCondition) {
+        typesLeaving++;
+        if (typesLeaving == locks.length) {
+          typesArrived = 0;
+          wayOutCondition.notifyAll();
+        } else {
+          while (typesLeaving < locks.length) {
+            wayOutCondition.wait();
+          }
+        }
+      }
+    }
+  }
+}
--- src/main/concurrent/Turnstile.java
+++ src/main/concurrent/Turnstile.java
@@ -5,6 +5,7 @@ public class Turnstile implements SynchroBuffer{
   private final int workerCount;
   private int round;
   private int workersCompleted;
+  private final Object mux;
 
   public Turnstile(int workerCount) throws IllegalArgumentException {
     if(workerCount <= 0) {
@@ -13,25 +14,30 @@ public class Turnstile implements SynchroBuffer{
     this.workerCount = workerCount;
     round = 0;
     workersCompleted = 0;
+    mux = new Object();
   }
 
   public int getWorkerCount() {
     return workerCount;
   }
 
-  public synchronized void waitUntilReady(String arg) throws InterruptedException {
-    waitUntilReady();
+  public void waitUntilReady(String arg) throws InterruptedException {
+    synchronized (mux) {
+      waitUntilReady();
+    }
   }
 
-  public synchronized void waitUntilReady() throws InterruptedException {
-    workersCompleted++;
-    int roundLocal = round;
-    if(workersCompleted == workerCount * (roundLocal+1)) {
-      round++;
-      notifyAll();
-    } else {
-      while (workersCompleted < workerCount * (roundLocal + 1)) {
-        wait();
+  public void waitUntilReady() throws InterruptedException {
+    synchronized (mux) {
+      workersCompleted++;
+      int roundLocal = round;
+      if (workersCompleted == workerCount * (roundLocal + 1)) {
+        round++;
+        mux.notifyAll();
+      } else {
+        while (workersCompleted < workerCount * (roundLocal + 1)) {
+          mux.wait();
+        }
       }
     }
   }

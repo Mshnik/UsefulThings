bc75c1d5bd773ab36d8a341674ecfa8d19b5f8ea
MShnik <Mgpshnik@gmail.com>
2016-03-31 17:17:04 -0400

    Added more supported Number types to NumExt, added tests for those

--- src/main/common/math/NumExt.java
+++ src/main/common/math/NumExt.java
@@ -1,6 +1,7 @@
 package common.math;
 
-
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.function.Function;
 
 //TODO - SPEC
@@ -22,10 +23,20 @@ public abstract class NumExt<T extends Number> {
       return (NumExt<T>) new IntExt((Integer)t);
     } else if (t instanceof Double) {
       return (NumExt<T>) new DoubleExt((Double)t);
+    } else if (t instanceof Long) {
+      return (NumExt<T>) new LongExt((Long)t);
     } else if (t instanceof Float) {
       return (NumExt<T>) new FloatExt((Float) t);
+    } else if (t instanceof Short)  {
+      return (NumExt<T>) new ShortExt((Short) t);
+    } else if (t instanceof Byte)  {
+      return (NumExt<T>) new ByteExt((Byte) t);
+    } else if (t instanceof BigInteger)  {
+      return (NumExt<T>) new BigIntExt((BigInteger) t);
+    } else if (t instanceof BigDecimal)  {
+      return (NumExt<T>) new BigDecimalExt((BigDecimal) t);
     } else {
-      throw new UnsupportedOperationException("Unknown numerical type " + t.getClass());
+      throw new UnsupportedOperationException("Unsupported numerical type " + t.getClass());
     }
   }
 
@@ -35,6 +46,10 @@ public abstract class NumExt<T extends Number> {
 
   public int asInt() {
     return val.intValue();
+  }
+
+  public long asLong() {
+    return val.longValue();
   }
 
   public float asFloat() {
@@ -51,7 +66,27 @@ public abstract class NumExt<T extends Number> {
   public abstract NumExt<T> multiply(T t2);
   public abstract NumExt<T> divide(T t2);
 
-  public static class IntExt extends NumExt<Integer> {
+  public static <T extends Number> T apply(T t1, Function<T, T> f) {
+    return wrap(t1).apply(f).asNumber();
+  }
+
+  public static <T extends Number> T add(T t1, T t2) {
+    return wrap(t1).add(t2).asNumber();
+  }
+
+  public static <T extends Number> T subtract(T t1, T t2) {
+    return wrap(t1).subtract(t2).asNumber();
+  }
+
+  public static <T extends Number> T multiply(T t1, T t2) {
+    return wrap(t1).multiply(t2).asNumber();
+  }
+
+  public static <T extends Number> T divide(T t1, T t2) {
+    return wrap(t1).divide(t2).asNumber();
+  }
+
+  private static class IntExt extends NumExt<Integer> {
 
     private IntExt(Integer val) {
       super(val);
@@ -78,7 +113,34 @@ public abstract class NumExt<T extends Number> {
     }
 
   }
-  public static class FloatExt extends NumExt<Float> {
+  private static class LongExt extends NumExt<Long> {
+
+    private LongExt(Long val) {
+      super(val);
+    }
+
+    public NumExt<Long> apply(Function<Long, Long> f) {
+      return wrap(f.apply(val));
+    }
+
+    public NumExt<Long> add(Long i) {
+      return wrap(val + i);
+    }
+
+    public NumExt<Long> subtract(Long i) {
+      return wrap(val - i);
+    }
+
+    public NumExt<Long> multiply(Long i) {
+      return wrap(val * i);
+    }
+
+    public NumExt<Long> divide(Long i) {
+      return wrap(val / i);
+    }
+
+  }
+  private static class FloatExt extends NumExt<Float> {
 
     private FloatExt(Float val) {
       super(val);
@@ -104,7 +166,7 @@ public abstract class NumExt<T extends Number> {
       return wrap(val / i);
     }
   }
-  public static class DoubleExt extends NumExt<Double> {
+  private static class DoubleExt extends NumExt<Double> {
 
     private DoubleExt(Double val) {
       super(val);
@@ -131,4 +193,114 @@ public abstract class NumExt<T extends Number> {
     }
 
   }
+  private static class ShortExt extends NumExt<Short> {
+
+    private ShortExt(Short val) {
+      super(val);
+    }
+
+    public NumExt<Short> apply(Function<Short, Short> f) {
+      return wrap(f.apply(val));
+    }
+
+    public NumExt<Short> add(Short i) {
+      return wrap((short)(val + i));
+    }
+
+    public NumExt<Short> subtract(Short i) {
+      return wrap((short)(val - i));
+    }
+
+    public NumExt<Short> multiply(Short i) {
+      return wrap((short)(val * i));
+    }
+
+    public NumExt<Short> divide(Short i) {
+      return wrap((short)(val / i));
+    }
+
+  }
+  private static class ByteExt extends NumExt<Byte> {
+
+    private ByteExt(Byte val) {
+      super(val);
+    }
+
+    public NumExt<Byte> apply(Function<Byte, Byte> f) {
+      return wrap(f.apply(val));
+    }
+
+    public NumExt<Byte> add(Byte i) {
+      return wrap((byte)(val + i));
+    }
+
+    public NumExt<Byte> subtract(Byte i) {
+      return wrap((byte)(val - i));
+    }
+
+    public NumExt<Byte> multiply(Byte i) {
+      return wrap((byte)(val * i));
+    }
+
+    public NumExt<Byte> divide(Byte i) {
+      return wrap((byte)(val / i));
+    }
+
+  }
+
+  private static class BigIntExt extends NumExt<BigInteger> {
+
+    private BigIntExt(BigInteger val) {
+      super(val);
+    }
+
+    public NumExt<BigInteger> apply(Function<BigInteger, BigInteger> f) {
+      return wrap(f.apply(val));
+    }
+
+    public NumExt<BigInteger> add(BigInteger i) {
+      return wrap(val.add(i));
+    }
+
+    public NumExt<BigInteger> subtract(BigInteger i) {
+      return wrap(val.subtract(i));
+    }
+
+    public NumExt<BigInteger> multiply(BigInteger i) {
+      return wrap(val.multiply(i));
+    }
+
+    public NumExt<BigInteger> divide(BigInteger i) {
+      return wrap(val.divide(i));
+    }
+
+  }
+  private static class BigDecimalExt extends NumExt<BigDecimal> {
+
+    private BigDecimalExt(BigDecimal val) {
+      super(val);
+    }
+
+    public NumExt<BigDecimal> apply(Function<BigDecimal, BigDecimal> f) {
+      return wrap(f.apply(val));
+    }
+
+    public NumExt<BigDecimal> add(BigDecimal i) {
+      return wrap(val.add(i));
+    }
+
+    public NumExt<BigDecimal> subtract(BigDecimal i) {
+      return wrap(val.subtract(i));
+    }
+
+    public NumExt<BigDecimal> multiply(BigDecimal i) {
+      return wrap(val.multiply(i));
+    }
+
+    public NumExt<BigDecimal> divide(BigDecimal i) {
+      return wrap(val.divide(i));
+    }
+
+  }
+
 }
--- src/tests/common/math/NumExtTest.java
+++ src/tests/common/math/NumExtTest.java
@@ -2,6 +2,9 @@ package common.math;
 
 import org.junit.Test;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 import static common.JUnitUtil.*;
 
 /**
@@ -9,25 +12,47 @@ import static common.JUnitUtil.*;
  */
 public class NumExtTest {
 
+  private void testIs5(NumExt<?> n) {
+    assertEquals(5, n.asInt());
+    assertEquals(5.0f, n.asFloat());
+    assertEquals(5.0, n.asDouble());
+    assertEquals(5L, n.asLong());
+  }
+
   @Test
   public void testWrapping() {
     NumExt<Integer> n = NumExt.wrap(5);
     assertEquals(5, n.asNumber());
-    assertEquals(5, n.asInt());
-    assertEquals(5.0f, n.asFloat());
-    assertEquals(5.0, n.asDouble());
+    testIs5(n);
 
     NumExt<Float> n2 = NumExt.wrap(5.0f);
     assertEquals(5.0f, n2.asNumber());
-    assertEquals(5, n2.asInt());
-    assertEquals(5.0f, n2.asFloat());
-    assertEquals(5.0, n2.asDouble());
+    testIs5(n2);
 
     NumExt<Double> n3 = NumExt.wrap(5.0);
     assertEquals(5.0, n3.asNumber());
-    assertEquals(5, n3.asInt());
-    assertEquals(5.0f, n3.asFloat());
-    assertEquals(5.0, n3.asDouble());
+    testIs5(n3);
+
+    NumExt<Long> n4 = NumExt.wrap(5L);
+    assertEquals(5L, n4.asNumber());
+    testIs5(n4);
+
+    NumExt<Short> n5 = NumExt.wrap((short)5);
+    assertEquals((short)5, n5.asNumber());
+    testIs5(n5);
+
+    NumExt<Byte> n6 = NumExt.wrap((byte)5);
+    assertEquals((byte)5, n6.asNumber());
+    testIs5(n6);
+
+    NumExt<BigInteger> n7 = NumExt.wrap(new BigInteger("5"));
+    assertEquals(new BigInteger("5"), n7.asNumber());
+    testIs5(n7);
+
+    NumExt<BigDecimal> n8 = NumExt.wrap(new BigDecimal("5.0"));
+    assertEquals(new BigDecimal("5.0"), n8.asNumber());
+    testIs5(n8);
+
   }
 
   @Test
